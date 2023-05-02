6d0427b287a8d39f9dcfad72cba4a1682158016d
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-05-03 00:08:37 +0200

    11ème commit

--- src/review/views/home.py
+++ src/review/views/home.py
@@ -1,9 +1,9 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.views import View
-from review.models import Ticket, Review
+from review.models import Ticket, Review, UserFollows
 from itertools import chain
-from django.db.models import CharField, Value
+from django.db.models import CharField, Value, Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -12,10 +12,24 @@ class HomeView(TemplateView):
     template_name = 'review/home.html'
 
     def get(self, request):
-        reviews = Review.objects.select_related("ticket").filter(user=self.request.user)
+        # users i follow
+        myusers = UserFollows.objects.filter(user=self.request.user)
+
+        # users who follow me
+        follusers = UserFollows.objects.filter(followed_user=self.request.user)
+
+        # reviews from users i follow , who follow me and my reviews
+        reviews = Review.objects.select_related("ticket").filter(Q(user=self.request.user) |
+                                                                  Q(user__in=myusers.values_list('followed_user', flat=True)) |
+                                                                  Q(user__in=follusers.values_list('user', flat=True)))
         reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
 
-        tickets = Ticket.objects.filter(user=self.request.user).exclude(id__in=reviews.values_list('ticket', flat=True))
+        # reviews from users i follow , who follow me and my reviews without reviews
+        tickets = Ticket.objects.filter(Q(user=self.request.user) |
+                                      Q(user__in=myusers.values_list('followed_user', flat=True)) |
+                                      Q(user__in=follusers.values_list('user', flat=True))).exclude(id__in=reviews.values_list('ticket', flat=True))
+
+
         tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
 
         post_list = sorted(
